b9a547c3eb16068d20a3e35a54a2d8e4a5a312cb
Amar Chatman <amarjerome@gmail.com>
2018-04-23 20:20:02 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ end
 
 def match(array)
 word_array = @word.split("")
-array.collect {|new_word| new_word.split.sort == word_array.join.split.sort}
+array.detect {|new_word| new_word.split.sort == word_array.join.split.sort}
 
 
 end
